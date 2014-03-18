544b698ef11c30eb32729b29fdccc5e5332ecf23
Jasmine <jasmine.feng@rutgers.edu>
2014-03-18 16:19:47 -0400

    Update haiku.c

--- haiku.c
+++ haiku.c
@@ -83,7 +83,7 @@ struct haiku* generate(){
 int main(){
 	
 	struct haiku* yay = generate();
-	
+	printf("Here is your haiku! Enjoy :)\n");
 	printf("%s\n%s\n%s\n", yay->line1, yay->line2, yay->line3);
 
 
