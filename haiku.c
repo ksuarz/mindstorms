7e3a5b497ec961a0b12451ffd94a8589f892eaad
Amanda <ohgoonetilleke@gmail.com>
2014-03-18 15:50:23 -0400

    Create haiku.c

---
+++ haiku.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct haiku {
+	char* firstLine;
+	char* secondLine;		
+	char* thirdLine;
+};
+
+char* getLine(int syllables){
+
+	if (syllables==5){
+
+	}
+	else if (syllables ==7){
+	
+	}
+
+
+}
